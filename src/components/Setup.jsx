3b1b6fd9da563e1ad31495ef0b7029f5bea5bf63
santi-misael <santiagoalamos2000@gmail.com>
2022-12-17 23:47:14 -0300

    evasión de errores al setup

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -1,34 +1,42 @@
 export default function Setup(){
     return (
         <div>
-            <form action="GET" class="contenedor formulario">
-                <fieldset class="fieldset">
-                    <label class="ml" for="tamanio">Tamaño</label>
-                    <select class="ml select" name="tamanio" id="tamanio">
-                        <option value=""selected disabled>Seleccione</option>
+            <form action="GET" className="contenedor formulario">
+                <fieldset className="fieldset">
+                    <label className="ml" 
+                    // for="tamanio"
+                    >Tamaño</label>
+                    <select className="ml select" name="tamanio" id="tamanio">
+                        <option value=""
+                        // selected
+                         disabled>Seleccione</option>
                     </select>
-                    <button type="button" class="ml btn btn-aplica">Aplicar</button>
-                    <div id="tamanio-registro" class="inline ml border">
+                    <button type="button" className="ml btn btn-aplica">Aplicar</button>
+                    <div id="tamanio-registro" className="inline ml border">
                         <p>4*4</p>
                     </div>
                 </fieldset>
-                <fieldset class="fieldset">
-                    <label class="ml" for="dificultad">Dificultad</label>
-                    <select class="ml select" name="dificultad" id="dificultad">
-                        <option value=""selected disabled>Seleccione</option>
+                <fieldset className="fieldset">
+                    <label className="ml" 
+                    // for="dificultad"
+                    >Dificultad</label>
+                    <select className="ml select" name="dificultad" id="dificultad">
+                        <option value=""
+                        // selected 
+                        disabled>Seleccione</option>
                     </select>
-                    <button type="button" class="ml btn btn-aplica">Aplicar</button>
-                    <div id="dificultad-registro" class="inline ml border">
+                    <button type="button" className="ml btn btn-aplica">Aplicar</button>
+                    <div id="dificultad-registro" className="inline ml border">
                         <p>3</p>
                     </div>
                 </fieldset>
 
-                <div class="alerta">
+                <div className="alerta">
                     <p>Se creará un sudoku de 4 x 4 y una dificultad de Nivel 3</p>
                 </div>
-                <div class="alerta">
+                <div className="alerta">
                     <p>Confirma su Elección?</p>    
-                    <button class="btn btn-submit" type="Sumbit"> OK </button>
+                    <button className="btn btn-submit" type="Sumbit"> OK </button>
                 </div>
             </form>
         </div>
