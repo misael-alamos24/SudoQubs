4d86899db0e9974e12db824f6903a73c32208209
Michellefsd <110755234+Michellefsd@users.noreply.github.com>
2022-12-17 22:12:19 -0300

    form de eleccion

--- src/components/Setup.jsx
+++ src/components/Setup.jsx
@@ -1,7 +1,36 @@
 export default function Setup(){
     return (
         <div>
-            {/* Soy Setup */}
+            <form action="GET" class="contenedor formulario">
+                <fieldset class="fieldset">
+                    <label class="ml" for="tamanio">Tamaño</label>
+                    <select class="ml select" name="tamanio" id="tamanio">
+                        <option value=""selected disabled>Seleccione</option>
+                    </select>
+                    <button type="button" class="ml btn btn-aplica">Aplicar</button>
+                    <div id="tamanio-registro" class="inline ml border">
+                        <p>4*4</p>
+                    </div>
+                </fieldset>
+                <fieldset class="fieldset">
+                    <label class="ml" for="dificultad">Dificultad</label>
+                    <select class="ml select" name="dificultad" id="dificultad">
+                        <option value=""selected disabled>Seleccione</option>
+                    </select>
+                    <button type="button" class="ml btn btn-aplica">Aplicar</button>
+                    <div id="dificultad-registro" class="inline ml border">
+                        <p>3</p>
+                    </div>
+                </fieldset>
+
+                <div class="alerta">
+                    <p>Se creará un sudoku de 4 x 4 y una dificultad de Nivel 3</p>
+                </div>
+                <div class="alerta">
+                    <p>Confirma su Elección?</p>    
+                    <button class="btn btn-submit" type="Sumbit"> OK </button>
+                </div>
+            </form>
         </div>
     )
 }
