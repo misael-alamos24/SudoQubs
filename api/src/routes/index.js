28fcc60373f528fb5cb41169c0138286bd24f6c5
santi-misael21 <santiagoalamos2000@gmail.com>
2023-06-14 17:01:10 -0300

    upd 14-6 navigation menu

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -8,6 +8,13 @@ const router = Router();
 router.use("/sudoku", sudokuRoutes);
 
 module.exports = router;
+
+router.get('/', (req, res)=>{
+    res.status(200).json({
+        message: 'Welcome to the sudoku API',
+        sudoku: '/sudoku',
+    })
+})
 // Configurar los routers
 
 
--- api/src/routes/sudoku.js
+++ api/src/routes/sudoku.js
@@ -7,6 +7,14 @@ router.get('/', async(req,res)=>{ // la ruta más inservible de todos los tiempo
 
     let {vertex} = req.query
 
+    if(!vertex) return res.status(202).json({
+        message: 'Put the size of sudoku next to vertex query. It can be any non-prime number (4, 6, 8, 9, etc.).',
+        vertex4x4: '/sudoku?vertex=4',
+        vertex6x6: '/sudoku?vertex=6',
+        vertex8x8: '/sudoku?vertex=8',
+        back: '/',
+    })
+
     let sudoku = generate(vertex)
 
     if(sudoku) return res.status(201).json(sudoku);
