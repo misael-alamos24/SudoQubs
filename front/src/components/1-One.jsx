1ba8f77de4806e9db8bd0b548543ca914e58d61f
Santiago Alamos <94709834+santi-misael21@users.noreply.github.com>
2023-07-20 02:21:01 -0300

    Update 1-One.jsx

--- front/src/components/1-One.jsx
+++ front/src/components/1-One.jsx
@@ -6,13 +6,19 @@ import Setup from "./Setup";
 export default function One(){
 
     return (
-        <div className="one">
-            <div className="component" id="">
-                <Setup/>
+        <div>
+            <div>
+                The function generators of sudokus are stored in a backend which there is to visit for reactivate it.
+                Visit this <a href="https://sudoqubs-backend.onrender.com/sudoku?vertex=4" target="_blank">link</a> only once to reactivate backend.
             </div>
-            <div className="component" id="sudoku">
-                <Sudoku/>
-            </div>    
+            <div className="one">
+                <div className="component" id="">
+                    <Setup/>
+                </div>
+                <div className="component" id="sudoku">
+                    <Sudoku/>
+                </div>    
+            </div>
         </div>
     )
 }
